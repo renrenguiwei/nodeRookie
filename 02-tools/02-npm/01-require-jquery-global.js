aa1ef8489c8c26a2ab0537facfb99d0a832e4634
ClausWong <wangjiaw@chanjet.com>
2023-01-03 23:08:30 +0800

    npm脚本寻址方式

---
+++ 02-tools/02-npm/01-require-jquery-global.js
@@ -0,0 +1,11 @@
+const $ = require('jquery')
+
+console.log($)
+
+// 脚本 yarn gulp_b
+// npm run gulp_b 会先去全局找，再从本地找
+
+/**
+ * 写法一："gulp_v": "gulp -v"
+ * 写法二："gulp_v": "./node_mobules/.bin/gulp -v"
+ */
