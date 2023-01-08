191b6d71aa676e8be02f44c5656dd84e1462c018
ClausWong <wangjiaw@chanjet.com>
2023-01-08 19:31:31 +0800

    npm 发包

--- 02-tools/02-npm/02-modules/thirdparty.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
-axios.get('https://5b87b1cd35589600143c1440.mockapi.io/api/v1/list')
-    .then(res => {
-        console.log(res.data)
-    })
-    .catch(err => {
-        console.log(err)
-    })
---
+++ 02-tools/02-npm/02-modules/thirdparty/thirdparty.js
@@ -0,0 +1,13 @@
+// const axios = require('axios')
+//
+// axios.get('https://5b87b1cd35589600143c1440.mockapi.io/api/v1/list')
+//     .then(res => {
+//         console.log(res.data)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+const myChunk = require('custom_claus')
+
+console.log(myChunk([1, 2, 3, 4]))
