d8fd32cba03486c1c1c12cff67d9c41db1556005
ClausWong <wangjiaw@chanjet.com>
2023-02-07 16:37:28 +0800

    get方法参数解析，返回状态

--- 01-npm/04-buildIn-module/02-http/get.js
+++ 01-npm/04-buildIn-module/02-http/get.js
@@ -1,3 +1,20 @@
 const logger = require('../../../utils/log')
+const http = require('http')
 
-logger.debug('claus')
+const server = http.createServer((request, response) => {
+    const url = request.url
+
+    response.writeHead(200, {
+        // 'content-type': 'text/plain'
+        'content-type': 'application/json;charset=utf-8'
+    })
+    // response.write('<div>claus</div>')
+    response.write(`{"url": "${url}"}`)
+    response.end()
+})
+
+server.listen(8999, () => {
+    console.log('localhost:8999')
+})
+
+// node --inspect --inspect-brk get.js
