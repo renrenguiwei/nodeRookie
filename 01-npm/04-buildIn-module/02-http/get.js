20223288988db250faf7a1f1404c1cdbd954a4b3
ClausWong <726787295@qq.com>
2023-02-08 10:25:46 +0800

    postman 模拟post数据至express服务

--- 01-npm/04-buildIn-module/02-http/get.js
+++ 01-npm/04-buildIn-module/02-http/get.js
@@ -3,7 +3,7 @@ const http = require('http')
 const https = require('https')
 const querystring = require('querystring')
 
-const server = http.createServer((request, response) => {
+const get = http.createServer((request, response) => {
 
     https.get('https://test-cloud.chanjet.com/cc/ue3fykjdphvz/dzq2dxmksi/entities/TaxIndustry/list', (result) => {
         let data = ''
@@ -22,9 +22,9 @@ const server = http.createServer((request, response) => {
     })
 })
 
-server.listen(8999, () => {
+get.listen(8999, () => {
     console.log('localhost:8999')
 })
 
-// node --inspect --inspect-brk server.js
+// node --inspect --inspect-brk get.js
 // contributions
---
+++ train-express-env/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var logger = require('../../utils/log')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/data', (req, res, next)=> {
+  logger.debug(req.body)
+  res.send('ok')
+})
+
+module.exports = router;
